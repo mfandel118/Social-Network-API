55cf1888aedd96ea49492f5f5bb2e5f8e6229202
Marissa Fandel <mfandel@alumni.nd.edu>
2022-04-05 19:35:35 -0600

    finsihed User model

--- models/User.js
+++ models/User.js
@@ -16,10 +16,12 @@ const userSchema = new Schema({
         match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please provide a valid email address']
     },
     thoughts: [{
-       // Thought model reference here
+        type: Schema.Types.ObjectId,
+        ref: 'thought'
     }],
     friends: [{
-        // Friend info reference here
+        type: Schema.Types.ObjectId,
+        ref: 'user'
     }]
 },
 {
