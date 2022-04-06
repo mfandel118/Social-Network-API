ab15963be9ef4e00ea2b16432038611cc57a762e
Marissa Fandel <mfandel@alumni.nd.edu>
2022-04-05 20:21:17 -0600

    updated userControllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,24 +6,63 @@ module.exports = {
     // See All Users
     getUsers(req, res) {
         User.find()
-          .then((users) => res.json(users))
-          .catch((err) => res.status(500).json(err));
+            .then((users) => res.json(users))
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
     },
     // See 1 User by ID
     getUserbyID(req, res) {
         User.findOne({ _id: req.params.userId })
-          .select('-__v')
-          .then((user) =>
-            !user
-              ? res.status(404).json({ message: 'No user with that ID' })
-              : res.json(user)
-          )
-          .catch((err) => res.status(500).json(err));
-      },
+            .select('-__v')
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'No user with that ID' })
+                    : res.json(user)
+            )
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
     // Create New User
     createUser(req, res) {
         User.create(req.body)
-          .then((dbUserData) => res.json(dbUserData))
-          .catch((err) => res.status(500).json(err));
-      },
+            .then((user) => res.json(user))
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
+    // Update a User
+    updateUser(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        )
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'No user exists with this ID!' })
+                    : res.json(user)
+            )
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
+    // Delete a User & all User's Thoughts
+    deleteUser(req, res) {
+        User.findOneAndDelete({ _id: req.params.userId })
+            .then((user) => {
+                if (!user) {
+                    return res.status(404).json({ message: 'No user exists with this ID!' })
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    }
 };
