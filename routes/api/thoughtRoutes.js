a01ee58be6ca310d3bc776cc4396062f707f4323
Marissa Fandel <mfandel@alumni.nd.edu>
2022-04-05 19:46:02 -0600

    started user & thought routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,13 @@
+// import express router
+const router = require('express').Router();
+// import controller functions
+
+
+// route: /api/thoughts
+
+
+// route: /api/thoughts/:thoughtId/reactions
+
+
+// export router
+module.exports = router;
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,12 @@
+// import express router
+const router = require('express').Router();
+// import controller functions
+
+// route: /api/users
+
+
+// route: /api/users/:userId/friends/:friendId
+
+
+// export router
+module.exports = router;
